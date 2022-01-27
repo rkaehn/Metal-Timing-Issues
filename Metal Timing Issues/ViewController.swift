4d8a3707d3b3ac04a6d40bd336c6762b712840cb
Raffael Kaehn <raffael.kaehn@gmail.com>
2022-01-27 18:01:19 +0100

    Initial commit

---
+++ Metal Timing Issues/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Metal Timing Issues
+//
+//  Created by Raffael Kaehn on 27.01.22.
+//
+
+import UIKit
+import MetalKit
+
+class ViewController: UIViewController {
+    var mtkView: MTKView!
+    
+    var commandQueue: MTLCommandQueue!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mtkView = MTKView()
+        view = mtkView
+        
+        mtkView.device = MTLCreateSystemDefaultDevice()!
+        mtkView.delegate = self
+        mtkView.preferredFramesPerSecond = 60
+        (mtkView.layer as! CAMetalLayer).maximumDrawableCount = 2
+        
+        commandQueue = mtkView.device!.makeCommandQueue()
+    }
+}
+
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+    
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+              let drawable = view.currentDrawable else { return }
+        commandBuffer.present(drawable)
+//        commandBuffer.present(drawable, afterMinimumDuration: 1.0/60.0)
+        commandBuffer.commit()
+    }
+}
